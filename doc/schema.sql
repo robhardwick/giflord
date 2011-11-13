44b955f67839515c1f4f13696d617bab5e1745cb
Rob Hardwick <robhardwick@gmail.com>
2011-11-13 21:44:01 +0000

    Fix schedule syntax in cron configuration

--- doc/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE images (
-    id INTEGER PRIMARY KEY,
-    url TEXT NOT NULL,
-    referrer TEXT NOT NULL,
-    hash TEXT NOT NULL,
-    created INTEGER NOT NULL,
-    updated INTEGER NOT NULL,
-    UNIQUE(url)
-);
