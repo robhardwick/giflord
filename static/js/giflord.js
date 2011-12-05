10b7dd9aab7f1e2a01ecaac718b7249b5691cbe1
Rob Hardwick <robhardwick@gmail.com>
2011-12-05 23:04:07 +0000

    Fix infinite scrolling and add loading styles

--- static/js/giflord.js
+++ static/js/giflord.js
@@ -1,81 +1,83 @@
 (function($) {
 
-    function getPos($win, $link, type) {
-        var win = (type == 'width') ? $win.width() : $win.height();
-        return parseInt((win / 2) - ((parseInt($link.data(type)) + 40) / 2), 10);
-    }
-
-    function loadImage($win, $overlay, $link) {
-
-        var $gif = $overlay.children('img')
-                .hide()
-                .attr('src', $link.attr('href'));
-
-        $.merge($gif, $overlay)
-            .animate({
-                width: $link.data('width') + 'px',
-                height: $link.data('height') + 'px',
-                left: getPos($win, $link, 'width'),
-                top: getPos($win, $link, 'height')
-            });
-
-        $gif.imagesLoaded(function() {
-                $gif.fadeIn('fast');
-            });
-
-    }
-
     $(document).ready(function() {
 
         var $loading = $('.loading').fadeIn('fast'),
             $win = $(window),
             $main = $('#main');
 
-        var $items = $main.find('a')
-            .click(function() {
+        function getPos($link, type) {
+            var win = (type == 'width') ? $win.width() : $win.height();
+            return parseInt((win / 2) - ((parseInt($link.data(type)) + 40) / 2), 10);
+        }
 
-                var $link = $(this),
-                    $overlay = $('<div/>')
-                        .addClass('overlay')
-                        .css({
-                            position: 'absolute',
-                            width: $link.data('width')+'px',
-                            height: $link.data('height')+'px'
-                        })
-                        .appendTo('body')
-                        .overlay({
-                            load: true,
-                            left: getPos($win, $link, 'width'),
-                            top: getPos($win, $link, 'height'),
-                            mask: {
-                                color: '#000',
-                                opacity: 0.4,
-                                loadSpeed: 200
-                            },
-                            onBeforeLoad: function(e) {
-                                $link.css('boxShadow', 'none');
-                            },
-                            onClose: function(e) {
-                                $overlay.remove();
-                            }
-                        });
+        function loadImage($overlay, $link) {
 
-                $('<img/>')
-                    .attr('src', $link.attr('href'))
-                    .attr('width', $link.data('width'))
-                    .attr('height', $link.data('height'))
-                    .click(function() {
-                        $link = $link.parent().next().children('a');
-                        if ($link.size() == 0) {
-                            $link = $items.filter(':first');
-                        }
-                        loadImage($win, $overlay, $link);
+            var $gif = $overlay.children('img')
+                    .hide()
+                    .attr('src', $link.attr('href'));
+
+            $.merge($gif, $overlay)
+                .animate({
+                    width: $link.data('width') + 'px',
+                    height: $link.data('height') + 'px',
+                    left: getPos($link, 'width'),
+                    top: getPos($link, 'height')
+                });
+
+            $gif.imagesLoaded(function() {
+                    $gif.fadeIn('fast');
+                });
+
+        }
+
+        function thumbClick(link) {
+
+            var $link = $(link),
+                $overlay = $('<div/>')
+                    .addClass('overlay')
+                    .css({
+                        position: 'absolute',
+                        width: $link.data('width')+'px',
+                        height: $link.data('height')+'px'
                     })
-                    .appendTo($overlay);
+                    .appendTo('body')
+                    .overlay({
+                        load: true,
+                        left: getPos($link, 'width'),
+                        top: getPos($link, 'height'),
+                        mask: {
+                            color: '#000',
+                            opacity: 0.4,
+                            loadSpeed: 200
+                        },
+                        onBeforeLoad: function(e) {
+                            $link.css('boxShadow', 'none');
+                        },
+                        onClose: function(e) {
+                            $overlay.remove();
+                        }
+                    });
 
-                return false;
+            $('<img/>')
+                .attr('src', $link.attr('href'))
+                .attr('width', $link.data('width'))
+                .attr('height', $link.data('height'))
+                .click(function() {
+                    $link = $link.parent().next().children('a');
+                    if ($link.size() == 0) {
+                        $link = $items.filter(':first');
+                    }
+                    loadImage($overlay, $link);
+                })
+                .appendTo($overlay);
 
-            });
+            return false;
+
+        }
+
+        var $items = $main.find('a')
+            .click(function() { return thumbClick(this); });
 
         $main.imagesLoaded(function($images) {
             $main
@@ -87,7 +89,37 @@
                 .fadeOut('fast', function() {
                     $main.find('li')
                         .fadeIn();
+                    $('#main').infinitescroll({
+                        navSelector: '.next',
+                        nextSelector: '.next',
+                        itemSelector: '#main li',
+                        bufferPx: 100,
+                        loading: {
+                            finishedMsg: null,
+                            img: '/static/img/loading.gif',
+                            msg: null,
+                            msgText: '',
+                        },
+                        pathParse: {
+                            join: function(page) {
+                                return '/' + page
+                            }
+                        }
+                    },
+                    function(items) {
+                        var $items = $(items)
+                            .addClass('show')
+                            .hide();
+                        $items.find('a')
+                            .click(function() { return thumbClick(this); });
+                        $items.imagesLoaded(function() {
+                            $main.masonry('appended', $items, true);
+                            $items.fadeIn('fast');
+                        });
+                    });
+
                 });
+
         });
 
     });
